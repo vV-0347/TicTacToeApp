252c26c5f636f36c31208b46a266a1ec57f2e802
vV-0347 <vidyuthsumanth1@gmail.com>
2026-04-21 09:51:58 +0530

    UC2: Toss to decide first player and assign symbols

--- src/org/example/TicTacToe.java
+++ src/org/example/TicTacToe.java
@@ -1,34 +1,45 @@
 package org.example;
 
+import java.util.Random;
+
 public class TicTacToe {
 
-    // 3x3 board
-    static char[][] board = new char[3][3];
+    static boolean isHumanTurn;
+    static char humanSymbol;
+    static char computerSymbol;
 
     public static void main(String[] args) {
-        initializeBoard();
-        printBoard();
+        tossAndAssignSymbols();
+        displayTossResult();
     }
 
-    // Initialize board with '-'
-    static void initializeBoard() {
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col++) {
-                board[row][col] = '-';
-            }
+    // Toss to decide who starts and assign symbols
+    static void tossAndAssignSymbols() {
+        Random rand = new Random();
+
+        // Generate 0 or 1
+        int toss = rand.nextInt(2);
+
+        if (toss == 0) {
+            isHumanTurn = true;
+            humanSymbol = 'X';
+            computerSymbol = 'O';
+        } else {
+            isHumanTurn = false;
+            humanSymbol = 'O';
+            computerSymbol = 'X';
         }
     }
 
-    // Print the board
-    static void printBoard() {
-        System.out.println("-------------");
-        for (int row = 0; row < 3; row++) {
-            System.out.print("| ");
-            for (int col = 0; col < 3; col++) {
-                System.out.print(board[row][col] + " | ");
-            }
-            System.out.println();
-            System.out.println("-------------");
+    // Display result of toss
+    static void displayTossResult() {
+        if (isHumanTurn) {
+            System.out.println("Human won the toss and plays first.");
+        } else {
+            System.out.println("Computer won the toss and plays first.");
         }
+
+        System.out.println("Human Symbol: " + humanSymbol);
+        System.out.println("Computer Symbol: " + computerSymbol);
     }
 }
